d49da3f9fd8fea49d33cc01d4580e113e1706da0
smkun <skunian@yahoo.com>
2024-05-13 15:01:50 -0400

    final saves

--- web/src/components/StripeContainer.js
+++ web/src/components/StripeContainer.js
@@ -1,16 +1,17 @@
-import { Elements } from "@stripe/react-stripe-js"
-import { loadStripe } from "@stripe/stripe-js"
-import React from "react"
-import PaymentForm from "./PaymentForm"
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import PaymentForm from "./PaymentForm";
 
-const PUBLIC_KEY = "pk_test_51PFzqTFBWDN7KFlS4tuNLa470ygLXgcY1ulz4i0CSo0KFS0wxha6CpSlYZ2tEJytHdCiM7bI9USI8XQPPVAF2dEL00Zwl64L0n"
+const PUBLIC_KEY =
+    "pk_test_51PFzqTFBWDN7KFlS4tuNLa470ygLXgcY1ulz4i0CSo0KFS0wxha6CpSlYZ2tEJytHdCiM7bI9USI8XQPPVAF2dEL00Zwl64L0n";
 
-const stripeTestPromise = loadStripe(PUBLIC_KEY)
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
 export default function StripeContainer() {
-	return (
-		<Elements stripe={stripeTestPromise}>
-			<PaymentForm />
-		</Elements>
-	)
+    return (
+        <Elements stripe={stripeTestPromise}>
+            <PaymentForm />
+        </Elements>
+    );
 }
